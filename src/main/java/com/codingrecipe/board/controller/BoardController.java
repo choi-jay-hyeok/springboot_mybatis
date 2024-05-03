43651daf99861b973c9deba7091b010b260506bf
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-05-03 23:32:35 +0900

    03_Save data into DB

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -22,6 +22,7 @@ public class BoardController {
     @PostMapping("/save")
     public String save(@ModelAttribute BoardDTO boardDTO) {
         System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
         return "index";
     }
 }
--- src/main/java/com/codingrecipe/board/repository/BoardRepository.java
+++ src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -1,7 +1,17 @@
 package com.codingrecipe.board.repository;
 
+import com.codingrecipe.board.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BoardRepository {
+
+    private final SqlSessionTemplate sql; // mapper 호출하는 용도
+
+    public void save(BoardDTO boardDTO) {
+        sql.insert("Board.save", boardDTO); // board-mapper.xml 에서 namespace 가 Board 이고, id 가 save 인 태그를 호출하겠다. 객체 여러개를 넘기고 싶으면 HashMap 사용
+    }
 }
