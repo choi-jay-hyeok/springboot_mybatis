bee0e7d81bcac849d45cfadd0129fbf51a5a9dec
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-05-03 22:56:34 +0900

    02_Create Controller, Service, Repository, mybatis

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,27 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller // controller 지정 - 자동으로 스프링빈 등록(내부에 @component 어노테이션이 있음)
+@RequiredArgsConstructor // final 이 붙은 필드의 생성자 생성 - 생성자 주입 방식
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
+    }
+}
---
+++ src/main/java/com/codingrecipe/board/dto/BoardDTO.java
@@ -0,0 +1,20 @@
+package com.codingrecipe.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter // getter 자동 생성
+@Setter // setter 자동 생성
+@ToString // '필드명=필드값' 자동 생성
+public class BoardDTO {
+
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits;
+    private String createdAt;
+
+}
